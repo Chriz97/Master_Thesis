504b9d5c53420dbbe1cdd79bef0b812ddfb93aa3
Christoph Mayer <114406924+Chriz97@users.noreply.github.com>
2023-11-02 17:52:14 +0100

    Update frame_times.py

--- frame_times.py
+++ frame_times.py
@@ -1,42 +1,62 @@
+import os
 import json
 import pandas as pd
 import datetime
-import os
 
-# Define the file path
-json_file_path = r'C:\Users\Christoph\Documents\CapFrameX\Captures\CapFrameX-BlackOps.exe-2023-10-30T233443.json'
+# Define the directory containing the JSON files
+json_directory = r'C:\Users\Christoph\Documents\CapFrameX\Captures'
 
-# Open and read the JSON file
+# List all JSON files in the directory
+json_files = [file for file in os.listdir(json_directory) if file.endswith('.json')]
+
+# Display the list of available JSON files
+print("Available JSON files:")
+for i, file in enumerate(json_files):
+    print(f"{i + 1}: {file}")
+
+# Prompt the user to select a file by index
+while True:
+    try:
+        selected_index = int(input("Enter the index of the JSON file you want to analyze: ")) - 1
+        if 0 <= selected_index < len(json_files):
+            selected_file = json_files[selected_index]
+            break
+        else:
+            print("Invalid index. Please enter a valid index.")
+    except ValueError:
+        print("Invalid input. Please enter a valid index.")
+
+# Construct the full file path for the selected JSON file
+json_file_path = os.path.join(json_directory, selected_file)
+
+# Extract the game name from the selected JSON file's name and remove ".exe"
+game_name = selected_file.split('-')[1].replace('.exe', '')
+if game_name == "cod":
+    game_name = "Modern Warfare III"
+
+# Open and read the selected JSON file
 with open(json_file_path, 'r') as json_file:
     data = json.load(json_file)
-    print(data.keys())
     x = data["Runs"][0]
     y = x["CaptureData"]
     frame_times = y["MsBetweenPresents"]
-    print(frame_times)
 
 df = pd.DataFrame(frame_times)
 df = df.rename(columns={0: "FrameTime"})
-print(df)
 
 now = datetime.datetime.now()
 timestamp = now.strftime("%m_%d_%H_%M")  # Example: "10_30_11_44"
 
-# Define the target directory and file name
+# Define the target directory and include the game name in the output_file_name
 output_directory = r"C:\Users\Christoph\Documents\Master Thesis\Frametimes"
-output_file_name = f"benchmark_{timestamp}.xlsx"
+output_file_name = f"benchmark_{game_name}_{timestamp}.xlsx"
 
-# Full file path
+# Full file path for the output
 output_file_path = os.path.join(output_directory, output_file_name)
 
 # Save the DataFrame to Excel
 df.to_excel(output_file_path, index=True)
 
 print(f"DataFrame saved to {output_file_path}")
-
-
-
-
-
 
 
