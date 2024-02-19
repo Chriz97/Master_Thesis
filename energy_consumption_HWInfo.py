2f97514c81a0870998d51e7e83040e50849f6566
Christoph Mayer <114406924+Chriz97@users.noreply.github.com>
2024-02-19 17:49:58 +0100

    Update energy_consumption_HWInfo.py

--- energy_consumption_HWInfo.py
+++ energy_consumption_HWInfo.py
@@ -5,54 +5,46 @@ from datetime import datetime
 
 csv_directory = r'C:\Program Files\HWiNFO64'
 
-
-# Function to list all CSV files and load the selected one
-
-
+# Function to list all CSV files and load the selected ones
 def list_and_select_csv(csv_directory):
     csv_files_with_dates = [(file, os.path.getctime(os.path.join(csv_directory, file))) for file in
                             os.listdir(csv_directory) if file.endswith('.CSV')]
 
-    # Check if there are any CSV files in the directory
     if not csv_files_with_dates:
         print("No CSV files found in the directory.")
-        return
+        return []
 
-    # Sort files by creation date, newest first
     csv_files_with_dates.sort(key=lambda x: x[1], reverse=True)
 
-    # Display the list of available CSV files with their creation dates
     print("Available CSV files:")
     for i, (file, ctime) in enumerate(csv_files_with_dates):
         date_str = datetime.fromtimestamp(ctime).strftime('%Y-%m-%d %H:%M:%S')
         print(f"{i + 1}: {file} (Created: {date_str})")
 
-    # Ask the user to select a file
-    selection = int(input("Enter the number of the CSV file you want to analyze: ")) - 1
-    if selection < 0 or selection >= len(csv_files_with_dates):
-        print("Invalid selection.")
-        return
+    selected_indices_input = input("Enter the numbers of the CSV files you want to analyze, separated by commas: ")
+    selected_indices = [int(index.strip()) - 1 for index in selected_indices_input.split(',') if index.strip().isdigit()]
 
-    selected_file_path = os.path.join(csv_directory, csv_files_with_dates[selection][0])
+    selected_files_paths = [os.path.join(csv_directory, csv_files_with_dates[i][0]) for i in selected_indices if 0 <= i < len(csv_files_with_dates)]
 
-    # Load the selected CSV file
+    return selected_files_paths
 
-    data = pd.read_csv(selected_file_path, usecols=['Date', 'Time', 'GPU Core Load [%]', 'GPU Power (Total) [W]'],
+def process_csv_file(file_path):
+    data = pd.read_csv(file_path, usecols=['Date', 'Time', 'GPU Core Load [%]', 'GPU Power (Total) [W]'],
                        encoding='cp1252')
     data = data.iloc[:-2]
+    data['GPU Core Load [%]'] = pd.to_numeric(data['GPU Core Load [%]'], errors='coerce')
+    data['GPU Power (Total) [W]'] = pd.to_numeric(data['GPU Power (Total) [W]'], errors='coerce')
     return data
 
+def save_df_to_excel(df, game_name, technology_name, upscaling_name, graphis_name, resolution_name, graphis_card_name):
+    now = datetime.now()
+    timestamp = now.strftime("%Y_%m_%d_%H_%M_%S_%f")[:-3]  # Including milliseconds
 
-# Creation of the Data Frame
-
-df = pd.DataFrame(list_and_select_csv(csv_directory))
-
-# Convert 'GPU Core Load [%]' and 'GPU Power (Total) [W]' columns to numeric
-df['GPU Core Load [%]'] = pd.to_numeric(df['GPU Core Load [%]'], errors='coerce')
-df['GPU Power (Total) [W]'] = pd.to_numeric(df['GPU Power (Total) [W]'], errors='coerce')
-
-now = datetime.now()
-timestamp = now.strftime("%m_%d_%H_%M_%S")  # Example: "10_30_11_44_59"
+    output_directory = r"C:\Users\Christoph\Documents\Master Thesis\Energy Consumption"
+    output_file_name = f"Energy_Consumption_{game_name}_{technology_name}_{upscaling_name}_{graphis_name}_{resolution_name}_{timestamp}_{graphis_card_name}.xlsx"
+    output_file_path = os.path.join(output_directory, output_file_name)
+    df.to_excel(output_file_path, index=False)
+    print(f"DataFrame saved to {output_file_path}")
 
 
 def check_benchmark():
@@ -82,16 +74,11 @@ def check_benchmark():
     graphis_card_name = graphics_card[graphics_card_index]
     return game_name, technology_name, upscaling_name, graphis_name, resolution_name, graphis_card_name
 
+selected_files_paths = list_and_select_csv(csv_directory)
+
 
 game_name, technology_name, upscaling_name, graphis_name, resolution_name, graphis_card_name = check_benchmark()
 
-output_directory = r"C:\Users\Christoph\Documents\Master Thesis\Energy Consumption"
-output_file_name = (f"Energy_Consumption_{game_name}_{technology_name}_{upscaling_name}_{graphis_name}_"
-                    f"{resolution_name}_{timestamp}_{graphis_card_name}.xlsx")
-
-output_file_path = os.path.join(output_directory, output_file_name)
-
-# Save the DataFrame to Excel
-df.to_excel(output_file_path, index=False)
-
-print(f"DataFrame saved to {output_file_path}")
+for file_path in selected_files_paths:
+    df = process_csv_file(file_path)
+    save_df_to_excel(df, game_name, technology_name, upscaling_name, graphis_name, resolution_name, graphis_card_name)
